71becf522ef9df52cb140d11b60acf6aeade357a
DESKTOP-12FBGF3\Joan <joancampusnet@gmail.com>
2023-04-09 07:41:53 +0200

    Agregar archivos de proyecto.

---
+++ DayOfTheWeek/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DayOfTheWeek
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a year");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write a month");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write a day");
+            int day = int.Parse(Console.ReadLine());
+            int dayofweek = DayOfTheWeek(year, month, day);
+            Console.WriteLine("The day of the week is:");
+            switch (dayofweek)
+            {
+                case 0: Console.WriteLine("Sunday"); break;
+                case 1: Console.WriteLine("Monday"); break;
+                case 2: Console.WriteLine("Tuesday"); break;
+                case 3: Console.WriteLine("Wednesday"); break;
+                case 4: Console.WriteLine("Thursday"); break;
+                case 5: Console.WriteLine("Friday"); break;
+                case 6: Console.WriteLine("Saturday"); break;
+                default: Console.WriteLine("Unknown"); break;
+            }
+        }
+        public static int DayOfTheWeek(int year, int month, int day)
+        {
+            if (month < 3)
+            {
+                month += 12;
+                year--;
+            }
+            int c = year / 100;
+            int y = year % 100;
+            int m = month;
+            int d = day;
+            int w = (int)((d + Math.Floor((13 * (m + 1)) / 5.0) + y + Math.Floor(y / 4.0) + Math.Floor(c / 4.0) + (5 * c)) % 7);
+            return w - 1;
+
+        }
+    }
+}
+
---
+++ Sorting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unordered list: ");
+            List<int> list = new List<int>(100);
+            for (int i = 0; i < 100; i++)
+            {
+                list.Add(Utils.random.Next());
+                Console.WriteLine(list[i]);
+            }
+
+            list = Sort(list);
+            Console.WriteLine("Ordered list: ");
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+        }
+
+        static public List<int> Sort(List<int> list)
+        {
+            if (list.Count <= 1)
+                return list;
+            int pivot = list.Count / 2;
+            int pivotValue = list[pivot];
+            list.RemoveAt(pivot);
+            List<int> smaller = new List<int>();
+            List<int> greater = new List<int>();
+            foreach (int element in list)
+            {
+                if (element <= pivotValue)
+                    smaller.Add(element);
+                else
+                    greater.Add(element);
+            }
+            List<int> sorted = new List<int>();
+            sorted.AddRange(Sort(smaller));
+            sorted.Add(pivotValue);
+            sorted.AddRange(Sort(greater));
+            return sorted;
+        }
+    }
+}
